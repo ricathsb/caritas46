44922c454bd829a19c8df7266a6a1e03906bf5ec
Hotbaen_070 <messijr.10hes@gmail.com>
2023-11-23 16:47:42 +0700

    PW

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage with Search Bar</title>
+    <title>Genshin Caritas</title>
     <link rel="stylesheet" href="style.css">
     <!-- <link rel="stylesheet" href="homepage/homepage.css"> -->
 </head>
